9c1ac067b61877537ef5617f785ec184ad2c672f
Milesq <wmilosz88@gmail.com>
2019-03-27 15:47:48 +0100

    Add router-link's

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -17,58 +17,76 @@
           </router-link>
         </article>
         <article>
+          <router-link to="/shop">
             <span class="menu__icon">
               <img src="@/assets/shop.svg" alt="Sklep z przedmiotami">
             </span>
             <span class="menu__content">sklep</span>
+          </router-link>
         </article>
         <article>
+          <router-link to="/plan">
             <span class="menu__icon">
               <img src="@/assets/notepad.svg" alt="plan edycji">
             </span>
             <span class="menu__content">plan edycji</span>
+          </router-link>
         </article>
         <article>
+          <router-link to="/events">
             <span class="menu__icon">
               <img src="@/assets/event.svg" alt="Wydarzenia dotyczące serwera">
             </span>
             <span class="menu__content">eventy</span>
+          </router-link>
         </article>
         <article>
+          <router-link to="/films">
             <span class="menu__icon">
               <img src="@/assets/movie.svg" alt="filmy z serwera">
             </span>
             <span class="menu__content">filmy z serwera</span>
+          </router-link>
         </article>
         <article>
+          <router-link to="/regulations">
             <span class="menu__icon">
               <img src="@/assets/settings.svg" alt="regulamin serwera">
             </span>
             <span class="menu__content">regulamin</span>
+          </router-link>
         </article>
         <article>
+          <router-link to="/contact">
             <span class="menu__icon">
               <img src="@/assets/contact.svg" alt="kontakt z administratorami">
             </span>
             <span class="menu__content">kontakt</span>
+          </router-link>
         </article>
         <article>
+          <router-link to="/sectors">
             <span class="menu__icon">
               <img src="@/assets/table-chart.svg" alt="sektory">
             </span>
             <span class="menu__content">podgląd sektorów</span>
+          </router-link>
         </article>
         <article>
+          <router-link to="/bans">
             <span class="menu__icon">
               <img src="@/assets/ban.svg" alt="Bany">
             </span>
             <span class="menu__content">bany</span>
+          </router-link>
         </article>
         <article>
+          <router-link to="/changes">
             <span class="menu__icon">
               <img src="@/assets/history.svg" alt="Ikonka historii zmian">
             </span>
             <span class="menu__content">zmiany</span>
+          </router-link>
         </article>
     </nav>
 </template>
